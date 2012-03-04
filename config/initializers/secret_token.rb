bd1e44099aa740114a4fa34c459c4eb4698c868d
Trey Mitra <shobhan.mitra@gmail.com>
2012-03-03 16:05:36 -0800

    inital commit 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Castingsf::Application.config.secret_token = '26ba794011a3b335fa8095b48cf56673760f913efe28a6d23af2c29039a958fe6509f3b876ca8ea5ea03972dec60fbc08a94056d42ec20830785d5fa02da752b'
